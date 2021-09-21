27f487293d8f23f5836637637b3f9f03d86af2dc
snap222 <PatileaDL@dulwich.org.uk>
2021-09-21 10:12:20 +0100

    Update simple_pin.py

--- pin/simple_pin.py
+++ pin/simple_pin.py
@@ -1,6 +1,22 @@
-a = input('type pin numbers:').split()
-for i in range(0,len(a)):
-    a[i] = int(a[i])
-    
-for i in range(0,len(a)-1):
-    print(a)
+
+def permutation(pin):
+ 
+    if len(pin) == 0:
+        return []
+ 
+    if len(pin) == 1:
+        return [pin]
+ 
+    current_perm = [] 
+    for i in range(len(pin)):
+       m = pin[i]
+ 
+       RemainingList = pin[0:i] + pin[i+1:len(pin)]
+ 
+       for p in permutation(RemainingList):
+            current_perm.append([m] + p)
+ 
+ 
+data = list('123')
+for p in permutation(data):
+    print(p)
